a7094544ef3c39753b2b1dac975c61a61d0906b4
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-09-04 07:34:02 +0200

    Completed assignment

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,8 +4,13 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   function increment() {
-    setCount(count + 1);
+    console.log(`before first setState: ${count}`);
+    setCount(count => count + 1);
+    console.log(`after first setState: ${count}`);
+    setCount(count => count + 1);
+    console.log(`after second setState: ${count}`);
   }
+  console.log(`in component: ${count}`);
 
   return <button onClick={increment}>I have been clicked {count} times</button>;
 }
